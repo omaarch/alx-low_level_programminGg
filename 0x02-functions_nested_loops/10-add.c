b97e4e44293817fc8f6e2941a404707b62b0e1ce
omaarch <125563563+omaarch@users.noreply.github.com>
2023-03-23 00:19:41 +0000

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 
-/**
- * add - add two integers
- * @a: First integer
- * @b: Second integer
- *
- * Return: The sum of a and b
- */
-int add(int a, int b)
-{
-	return (a + b);
-}
+
+	/**
+	 * add - adds two integers and returns the result
+	 * @a: int to be added to b
+	 * @b: int to be added to a
+	 * Return: sum of a and b
+	 */
+	int add(int a, int b)
+	{
+		int c;
+
+
+		c = a + b;
+
+
+		return (c);
+	}
