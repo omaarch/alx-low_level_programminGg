c7bc0768b653151cb8a0c53f2e36e86227f76832
omaarch <125563563+omaarch@users.noreply.github.com>
2023-03-23 00:20:23 +0000

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,21 +1,34 @@
-#include "holberton.h"
 #include <stdio.h>
+	#include "main.h"
 
-/**
- * print_to_98 - print all integers from given value to 98 (inclusive)
- * @n: The start of the range
- *
- * Return: void
- */
-void print_to_98(int n)
-{
-	if (n == 98)
+
+	/**
+	 * print_to_98 - prints all natural numbers from n to 98,
+	 * followed by a new line
+	 * @n: print from this number
+	 */
+	void print_to_98(int n)
 	{
-		printf("%d\n", n);
+		int i, j;
+
+
+		if (n <= 98)
+		{
+			for (i = n; i <= 98; i++)
+			{
+				if (i != 98)
+					printf("%d, ", i);
+				else if (i == 98)
+					printf("%d\n", i);
+			}
+		} else if (n >= 98)
+		{
+			for (j = n; j >= 98; j--)
+			{
+				if (j != 98)
+					printf("%d, ", j);
+				else if (j == 98)
+					printf("%d\n", j);
+			}
+		}
 	}
-	else
-	{
-		printf("%d, ", n);
-		print_to_98((n < 98) ? n + 1 : n - 1);
-	}
-}
